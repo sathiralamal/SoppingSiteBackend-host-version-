51a048425baf7cfc7fc9707ccd86b77e1fa3e425
KevGomez <52691186+KevGomez@users.noreply.github.com>
2020-06-20 12:48:24 +0530

    Fixed

--- src/views/pages/Payment/cardPayment.js
+++ src/views/pages/Payment/cardPayment.js
@@ -104,9 +104,9 @@ class cardPayment extends Component {
 
         axios.post(global.backend+'/payment/addCardPayment',newCardPayment, options)
             .then(res=>{console.log('Add new payment :'+res.data)
-                    var protection="Confirm";
+                    wait var protection="Confirm";
                     window.location.href= `/payConfirm?protection=${protection}`;
-                    )}
+                       })
             .catch(err=>console.log('Error!! unsuccessful :'+err.data));
     }
 
